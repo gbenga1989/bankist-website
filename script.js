f148b18f85a0609bc6b3a13c4810f79ae73385c9
gbenga <odetoyinbog@gmail.com>
2025-05-01 23:19:36 +0100

    1-3

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden,11,2');
+  modal.classList.remove('hidden,11,2,3');
   overlay.classList.remove('hidden');
 };
 
